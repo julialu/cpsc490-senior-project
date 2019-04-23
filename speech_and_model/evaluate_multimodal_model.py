45f7a8c6fe168da586ef67566c9f5ac6de0990cd
Roland Huang <rhuang97@gmail.com>
2019-04-23 18:49:05 +0000

    Fix bugs in evluate_multimodal_model

--- speech_and_model/evaluate_multimodal_model.py
+++ speech_and_model/evaluate_multimodal_model.py
@@ -21,7 +21,7 @@ video_data = np.load("../matrices/fullbody_img_vl.npy", mmap_mode='r')
 print("video data loaded with shape:", video_data.shape)
 
 audio_data = np.load("../matrices/validation_2A_S_predictors.npy", mmap_mode='r')
-print("video data loaded with shape:", audio_data.shape)
+print("audio data loaded with shape:", audio_data.shape)
 
 ##### evaluate data
 
@@ -33,10 +33,10 @@ frames_per_annotation = 4
 
 #custom loss function
 def batch_CCC(y_true, y_pred):
-    CCC = uf.CCC(y_true, y_pred)
-    CCC = CCC /float(batch_size)
-    CCC = 1-CCC
-    return CCC
+	CCC = uf.CCC(y_true, y_pred)
+	CCC = CCC /float(batch_size)
+	CCC = 1-CCC
+	return CCC
 
 MODEL = '../models/multimodal_model.hdf5'
 #load classification model and latent extractor
@@ -46,37 +46,37 @@ print 'Using model ' + MODEL
 
 def predict_datapoint(audio, video, target):
 
-    final_pred = []
-    #compute prediction until last frame
-    start = 0
-    end = 0
-    audio_predictors = []
-    video_predictors = []
+	final_pred = []
+	#compute prediction until last frame
+	start = 0
+	end = 0
+	audio_predictors = []
+	video_predictors = []
 
-    # cut audio and video into slices for prediction
-    while start < (len(target)-SEQ_LENGTH):
-        end = start + SEQ_LENGTH
-        audio_temp = audio[start*frames_per_annotation:end*frames_per_annotation]
-        video_temp = video[start:end]
-        audio_predictors.append(audio_temp)
-        video_predictors.append(video_temp)
+	# cut audio and video into slices for prediction
+	while start < (len(target)-SEQ_LENGTH):
+		end = start + SEQ_LENGTH
+		audio_temp = audio[start*frames_per_annotation:end*frames_per_annotation]
+		video_temp = video[start:end]
+		audio_predictors.append(audio_temp)
+		video_predictors.append(video_temp)
 
-        start = end
+		start = end
 
-    audio_predictors = np.array(audio_predictors)
-    video_predictors = np.array(video_predictors)
-    predictions = valence_model.predict([audio_predictors, video_predictors])
-    predictions = predictions.reshape(-1)
+	audio_predictors = np.array(audio_predictors)
+	video_predictors = np.array(video_predictors)
+	predictions = valence_model.predict([audio_predictors, video_predictors])
+	predictions = predictions.reshape(-1)
 
-    #compute prediction for last frame
-    audio_last = audio[-int(SEQ_LENGTH*frames_per_annotation):]
-    video_last = video[-SEQ_LENGTH:]
+	#compute prediction for last frame
+	audio_last = audio[-int(SEQ_LENGTH*frames_per_annotation):]
+	video_last = video[-SEQ_LENGTH:]
 
-    last_pred = valence_model.predict([audio_last, video_last]).reshape(-1)
-    missing_samples = len(target) - predictions.shape[0]
-    last_pred = last_pred[-missing_samples:]
-    final_pred = np.concatenate((predictions, last_pred))
-    return final_pred
+	last_pred = valence_model.predict([audio_last[np.newaxis], video_last[np.newaxis]]).reshape(-1)
+	missing_samples = len(target) - predictions.shape[0]
+	last_pred = last_pred[-missing_samples:]
+	final_pred = np.concatenate((predictions, last_pred))
+	return final_pred
 
 sbj_n = range(1,11)
 str_n = [1]
@@ -85,14 +85,15 @@ name_format = 'Subject_{0}_Story_{1}'
 annotations_path = '../dataset/Validation/Annotations/'
 model_output_path = '../model_predictions/Validation/'
 
+train_labels = np.load('../matrices/training_2A_S_target.npy')
 start = 0
 end = 0
 for subject in sbj_n:
 	for story in str_n:
 		name = name_format.format(subject, story)
 		# count number of frames in original 
-		annotations = np.loadtxt(annotations_path + name + '.csv')
-		num_frames = len(annotations, skiprows=1)
+		annotations = np.loadtxt(annotations_path + name + '.csv', skiprows=1)
+		num_frames = len(annotations)
 		print name + ': {} frames'.format(num_frames)
 
 		# extract correct number of frames
@@ -106,12 +107,20 @@ for subject in sbj_n:
 
 		predictions = predict_datapoint(audio_slice, video_slice, label_slice)
 
+		target_mean = np.mean(train_labels)
+		target_std = np.std(train_labels)
+		final_pred = uf.f_trick(predictions, target_mean, target_std)
+
+		#apply butterworth filter
+		b, a = butter(3, 0.01, 'low')
+		final_pred = filtfilt(b, a, final_pred)
+
 		# output to csv file
-		preds = { 'valence': predictions }
+		preds = { 'valence': final_pred }
 		df = pd.DataFrame(preds, columns= ['valence'])
 
 		# change this folder for different models
 		df.to_csv(model_output_path + name + '.csv', index=None, header=True)
 
-		print '{} ccc {}'.format(name, ccc2(label_slice, predictions))
+		print '{} ccc {}'.format(name, ccc2(label_slice, final_pred))
 		start = end
