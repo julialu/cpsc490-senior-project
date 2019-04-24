b7b5496309734fa70e808179edd0d72617be075f
Roland Huang <rhuang97@gmail.com>
2019-04-24 17:36:03 -0400

    Add test to extract audio script

--- extract_audio.sh
+++ extract_audio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# make an audio folder under Training and Validation, then run this script
+# make an audio folder under Training, Validation, and Test, then run this script
 
 for i in dataset/Training/Videos/*.mp4;
   do name=`echo $i | cut -d'.' -f1`;
@@ -9,6 +9,11 @@ for i in dataset/Training/Videos/*.mp4;
 done
 
 for i in dataset/Validation/Videos/*.mp4;
+  do name=`echo $i | cut -d'.' -f1`;
+  echo $name;
+  ffmpeg -i "$i" -ab 160000 -ac 1 -ar 16000 -vn -f wav "${name/Videos/audio}.wav";
+
+for i in dataset/Test/Videos/*.mp4;
   do name=`echo $i | cut -d'.' -f1`;
   echo $name;
   ffmpeg -i "$i" -ab 160000 -ac 1 -ar 16000 -vn -f wav "${name/Videos/audio}.wav";
